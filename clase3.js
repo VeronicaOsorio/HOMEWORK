66590bb193d98f014659302b798728e9770315c7
vero <vero@git.com>
2024-02-14 15:29:40 -0500

    ,

--- clase3.js
+++ clase3.js
@@ -31,7 +31,7 @@ console.log (oldRegularFunction(2, 3))
 console.log( newRegularFunction(2,3))
 console.log(arrowFunction(2,3))
 
-//ejercicio
+//ejercicio//////////////////////
 
 const nuevaFuncion = function  (a) {
     if (a % 2 == 0) {
@@ -52,3 +52,19 @@ const funcionFlecha = (a) => {
 }
 
 console.log(funcionFlecha(3))
+
+//diferencias: alcance (local o global), forma de escribirla
+//funciones de flecha tienen un contexto local, si invoco algo dentro, solo mira lo que se encuentre adentro
+// no se puede invocar la super en flecha
+// No tengo acceso a los parametros / argumentos
+/* 
+
+SOBRECARGA
+Cambiar los parametros de una función (solo funciona con las regulares)
+const nobre = (parrants)
+nobre (1)
+nobre(1,2)
+nobre(1,2,3)- permite ir cambiando el numero de parametros
+
+##PRINCIPIO DE RESPONSABILIDAD UNICA
+*/
