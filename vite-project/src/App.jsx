77b1f3f092d7d71ca9b89b1cb6db3a8023859829
vero <vero@git.com>
2024-04-28 18:02:59 -0500

    s

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,7 @@ import FirstApp from "./components/FirstApp"
 function App () {
     return (
         <>
-            <FirstApp title = "Second App"/>
+            <FirstApp title = "Segundo"/>
         </>
     )
 }
